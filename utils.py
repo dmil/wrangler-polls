620ce6134765d0f0f426478e78672c7a23789a84
Al Johri <al.johri@gmail.com>
2018-10-13 06:48:06 -0400

    add charts to pvi stuff notebook, update utils script to download pres_results data

--- utils.py
+++ utils.py
@@ -1,30 +1,65 @@
+import io
+import us
+import csv
+import itertools
+import requests
 import pandas as pd
 import numpy as np
 
-def download_pres_results():
-	# TODO: read from daily kos
-	dkos = pd.read_csv('dailykos_pres_results.csv')
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
-	df = pd.DataFrame(dkos[dkos.columns[0]].iloc[1:])
-	df['state'] = df['Year']
-	del df['Year']
+# Sheet 1: 29622862
+# Sheet 2: 1024689221
 
-	dfs = []
-	for i in range(1,50,4):
-		year_df = dkos[dkos.columns[range(i,i+4)]]
-		year = int(year_df.columns[0])
-		year_df = year_df.iloc[1:]
-		year_df.columns = ['DEM','REP','PVI','VAL']
-		year_df = year_df.fillna('')
-		year_df['year'] = year
-		# year_df['PVI'] = year_df['PVI'].astype(str) + year_df['VAL'].astype(str)
-		del year_df['VAL']
-		del year_df['PVI']
-		year_df['state'] = df['state']
-		dfs.append(year_df)
+response = requests.get("https://docs.google.com/spreadsheets/d/1D-edaVHTnZNhVU840EPUhz3Cgd7m39Urx7HM8Pq6Pus/export?format=csv&id=1D-edaVHTnZNhVU840EPUhz3Cgd7m39Urx7HM8Pq6Pus&gid=1024689221")
 
-	results_df = pd.concat(dfs)
-	results_df['DEM'] = pd.to_numeric(results_df['DEM'], downcast='float')
-	results_df['REP'] = pd.to_numeric(results_df['REP'], downcast='float')
-	results_df.to_csv('pres_results.csv', index=False)
-	return results_df
+output = []
+
+with io.StringIO(response.text) as f:
+	reader = csv.reader(f)
+	
+	header1 = next(reader)
+
+	year_start_indicies = []
+	for i, col in enumerate(header1):
+		# print(i, repr(col))
+		if col == "1952": break
+		if col.startswith('20') or col.startswith('19'):
+			year_start_indicies.append((col, i,))
+
+	# print(year_start_indicies)
+
+	header2 = next(reader)
+
+	for row in reader:
+
+		# end of states, starts regions next
+		if row[0] == '': break
+
+		for ((year, index), (next_year, next_index)) in pairwise(year_start_indicies):
+			if row[0] == 'Nationwide':
+				state = 'US'
+			elif row[0] == 'Washington DC':
+				state = 'DC'
+			else:
+				state = us.states.lookup(row[0]).abbr
+			out = {
+				'year': int(year), 
+				'state': state,
+				'dem': row[index],
+				'rep': row[index+1],
+				# 'pvi': row[next_index-2] + row[next_index-1],
+				}
+			# print(out)
+			output.append(out)
+
+output = sorted(output, key=lambda x: (-x['year'], x['state']))
+
+with open('pres_results.csv', 'w') as f: 
+	writer = csv.DictWriter(f, fieldnames=output[0].keys())
+	writer.writeheader()
+	writer.writerows(output)
