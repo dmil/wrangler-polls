17679edbd8d39973b7e8d5fca0d04fd55455cd1a
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2018-10-13 01:04:53 -0400

    update utils

--- utils.py
+++ utils.py
@@ -17,14 +17,14 @@ def download_pres_results():
 		year_df.columns = ['DEM','REP','PVI','VAL']
 		year_df = year_df.fillna('')
 		year_df['year'] = year
-		year_df['PVI'] = year_df['PVI'].astype(str) + year_df['VAL'].astype(str)
+		# year_df['PVI'] = year_df['PVI'].astype(str) + year_df['VAL'].astype(str)
 		del year_df['VAL']
+		del year_df['PVI']
 		year_df['state'] = df['state']
 		dfs.append(year_df)
 
 	results_df = pd.concat(dfs)
 	results_df['DEM'] = pd.to_numeric(results_df['DEM'], downcast='float')
 	results_df['REP'] = pd.to_numeric(results_df['REP'], downcast='float')
-	results_df['d_margin'] = results_df['DEM'] - results_df['REP']
 	results_df.to_csv('pres_results.csv', index=False)
 	return results_df
